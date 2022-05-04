1b91f137e0d1d13461254f9008911030885431da
Nicolas BENAIS <nicolasbenais@MacBook-Air-de-Nicolas.local>
2022-05-04 14:53:07 +0200

    issue owner avatar

---
+++ src/pages/Home.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://lereacteur-vinted-api.herokuapp.com/offers"
+        );
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  });
+  return isLoading === true ? (
+    <div>Loading...</div>
+  ) : (
+    <div>
+      {data.offers.map((item) => {
+        return (
+          <div
+            key={item._id}
+            onClick={() => {
+              console.log(item);
+            }}
+          >
+            <div className="owner">
+              {item.owner.account.username}
+              <img
+                src={item.owner.account.avatar.asset_id}
+                alt="Owner avatar"
+              />
+            </div>
+            {item.product_details.map((product) => {
+              return <div>{}</div>;
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
---
+++ src/pages/Offer.js
@@ -0,0 +1,8 @@
+import { useParams } from "react-router-dom";
+
+const Product = () => {
+  const params = useParams();
+  return <div>Product : {params.productId}</div>;
+};
+
+export default Product;

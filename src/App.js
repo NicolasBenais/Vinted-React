a2e14ed6046b7f736cad0ed55b94c9f6695174f9
Nicolas BENAIS <nicolasbenais@MacBook-Air-de-Nicolas.local>
2022-05-05 00:07:51 +0200

    issue import of components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Pages
 import Home from "./pages/Home";
@@ -8,9 +8,6 @@ import Offer from "./pages/Offer";
 function App() {
   return (
     <Router>
-      <nav>
-        <Link to="/home"> Home</Link>
-      </nav>
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/offer/:id" element={<Offer />} />

42317c26f270fdd5091146716cbd1b3ee9c803f4
Nicolas BENAIS <nicolasbenais@MacBook-Air-de-Nicolas.local>
2022-05-05 02:01:20 +0200

    final commit for today

--- src/components/Home-Header.js
+++ src/components/Home-Header.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className="home_header">
       <Link to={"/home"}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
